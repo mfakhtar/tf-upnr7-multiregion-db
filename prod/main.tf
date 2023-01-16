9005f4bb704502ec2c89f2f0d6884cddc7a2db4f
“Fawaz” <terraform-edu@users.noreply.github.com>
2023-01-16 10:56:38 +0530

    Added default value of db username and password as null

--- prod/main.tf
+++ prod/main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  } 
+  }
+/*
+  backend "s3" {
+    # This backend configuration is filled in automatically at test time by Terratest. If you wish to run this example
+    # manually, uncomment and fill in the config below.
+
+    # bucket         = "<YOUR S3 BUCKET>"
+    # key            = "<SOME PATH>/terraform.tfstate"
+    # region         = "us-east-2"
+    # dynamodb_table = "<YOUR DYNAMODB TABLE>"
+    # encrypt        = true
+  }
+}
+*/
 provider "aws" {
   region = "ap-south-1"
   alias  = "primary"
@@ -10,11 +33,13 @@ provider "aws" {
 
 module "mysql_primary" {
   source = "../modules"
+
   providers = {
-    aws = aws.replica
+    aws = aws.primary
   }
 
-  db_name     = "prod_db"
+  db_name     = var.db_name
+
   db_username = var.db_username
   db_password = var.db_password
 
@@ -23,10 +48,12 @@ module "mysql_primary" {
 }
 
 module "mysql_replica" {
-    source = "../modules"
-    providers = {
-    aws = aws.primary
+  source = "../modules"
+
+  providers = {
+    aws = aws.replica
   }
+
   # Make this a replica of the primary
   replicate_source_db = module.mysql_primary.arn
 }
